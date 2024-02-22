ca67a1a55c75ebaa9d8a5ba9ff626e67f48e970e
JustinLawrenceMS <justin@thechubbytech.com>
2024-02-22 17:52:07 -0600

    build - add template js -todo configure webpack

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import $ from 'jquery'
+
+window.$ = window.jQuery = $;
+
+import browser from './assets/js/browser.min.js'
+import breakpoints from './assets/js/breakpoints.min.js'
+import * as util from './assets/js/util.js'
+import * as main from './assets/js/main.js'
+
+
+window.util = util;
+window.main = main;
+window.browser = browser;
+window.breakpoints = breakpoints;
 
 createApp(App).mount('#app')
