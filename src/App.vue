f30f9d50284298e72af40f880c10f02d61032db7
JustinLawrenceMS <justin@thechubbytech.com>
2024-02-24 08:55:29 -0600

    feat - add chatbox

--- src/App.vue
+++ src/App.vue
@@ -374,4 +374,30 @@ print 'It took ' + i + ' iterations to sort the deck.';</code></pre>
 						</div>
 					</div>
   </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ChatBox/>
 </template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import ChatBox from './components/ChatBox.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    ChatBox
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
