28b639a2cedc492757cda796d7574ffecf60a784
JustinLawrenceMS <justin@thechubbytech.com>
2024-02-25 21:08:11 -0600

    style - resize and float texts

--- src/main.js
+++ src/main.js
@@ -1,20 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import $ from 'jquery'
+import { createApp } from "vue";
+import App from "./App.vue";
+import $ from "jquery";
 
 window.$ = window.jQuery = $;
 
-import "./assets/css/main.css"
-import breakpoints from "./assets/js/breakpoints.min.js"
-//import "./assets/js/browser.min.js"
-import "./assets/js/util.js"
-import "./assets/js/main.js"
+import "./assets/css/main.css";
+import breakpoints from "./assets/js/breakpoints.min.js";
+import "./assets/js/util.js";
+import "./assets/js/main.js";
 
 window.breakpoints = breakpoints;
 
-import PerfectScrollbar from 'vue3-perfect-scrollbar'
-import 'vue3-perfect-scrollbar/dist/vue3-perfect-scrollbar.css'
+import PerfectScrollbar from "vue3-perfect-scrollbar";
+import "vue3-perfect-scrollbar/dist/vue3-perfect-scrollbar.css";
 
-const app = createApp(App)
-app.use(PerfectScrollbar)
-app.mount('#app')
+const app = createApp(App);
+app.use(PerfectScrollbar);
+app.mount("#app");
